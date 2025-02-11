d55b08405343dbe02736808b337c686576b330f0
pmkgit22 <pmkdevops22@gmail.com>
2025-02-11 14:27:30 +0530

    this is nani create servers

--- CreateEc2serRecord.sh
+++ CreateEc2serRecord.sh
@@ -20,9 +20,9 @@ for i in $@
 do
     if [[ $i == "mongodb" || $i == "mysql"]]
     then
-    INSTANCE_TYPE="t3.medium"
+        INSTANCE_TYPE="t3.medium"
     else
-    INSTANCE_TYPE="t2.medium" 
+        INSTANCE_TYPE="t2.micro" 
     fi
     echo "creating $i instance "
     IP_ADDRESS=$(aws ec2 run-instances --imageid $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --query "Instances[0].InstanceId" --output text)
