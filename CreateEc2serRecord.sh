7ff578b238f57970256d1b16c8cbb25f4f7a8ccb
pmkgit22 <pmkdevops22@gmail.com>
2025-02-11 15:15:41 +0530

    this is nani create servers

--- CreateEc2serRecord.sh
+++ CreateEc2serRecord.sh
@@ -12,27 +12,34 @@ do
     if [[ $i == "mongodb" || $i == "mysql" ]]; then
         INSTANCE_TYPE="t3.medium"
     else
-        INSTANCE_TYPE="t2.micro" 
+        INSTANCE_TYPE="t2.micro"
     fi
-    echo "creating $i instance "
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --query "Instances[0].InstanceId" --output text)
-    
-    echo "created $i instance : $INSTANCE_ID "
-    
-    # NOTE: The above command returns an instance ID, not the public IP address.
-    # If you want to point your DNS to the instance's public IP, you'll need to retrieve it after the instance is running.
 
-    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
-    {
-        "Changes": [{
-            "Action": "CREATE",
-            "ResourceRecordSet": {
-                "Name": "'$i.$DOMAIN_NAME'",
-                "Type": "A",
-                "TTL": 300,
-                "ResourceRecords": [{ "Value": "'$INSTANCE_ID'" }]
-            }
-        }]
-    }
-    '
+    echo "creating $i instance"
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --query "Instances[0].InstanceId" --output text)
+    echo "created $i instance: $INSTANCE_ID"
+
+    # Wait for the instance to be running
+    aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+
+    # Retrieve the public IP address of the instance
+    PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    echo "Public IP for instance $INSTANCE_ID is: $PUBLIC_IP"
+
+    # Create the Route 53 DNS record using the public IP address
+    read -r -d '' CHANGE_BATCH <<EOF
+{
+    "Changes": [{
+        "Action": "CREATE",
+        "ResourceRecordSet": {
+            "Name": "$i.$DOMAIN_NAME",
+            "Type": "A",
+            "TTL": 300,
+            "ResourceRecords": [{ "Value": "$PUBLIC_IP" }]
+        }
+    }]
+}
+EOF
+
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch "$CHANGE_BATCH"
 done
